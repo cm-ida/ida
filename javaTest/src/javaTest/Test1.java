d9608de80dcb6ac2d9d75d73217be5e3ff8b4b7c
cm-ida <ida@cherimo.co.jp>
2019-07-31 01:04:27 +0900

    付録問題第１問

--- javaTest/src/javaTest/Test1.java
+++ javaTest/src/javaTest/Test1.java
@@ -3,6 +3,23 @@ package javaTest;
 public class Test1 {
 
 	public static void main(String[] args) {
+		int a;
+		a=1;
+
+
+		int b;
+		b=2;
+
+		int c;
+		c=a+b;
+
+		c=c+1;
+
+
+
+
+
+
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
