3fa41bbfa0f2dc82b6ce1c37fd5145a6c6d5a2a5
cm-ida <ida@cherimo.co.jp>
2019-07-31 20:01:30 +0900

    付録問題第1問

--- javaTest/src/javaTest/Test1.java
+++ javaTest/src/javaTest/Test1.java
@@ -12,11 +12,24 @@ public class Test1 {
 
 		int c;
 		c=a+b;
+		
+		System.out.println("c=a+b");
 
 		c=c+1;
 
 
-
+		int nenrei;
+		nenrei=0;
+		
+		int kakaku;
+		kakaku=0;
+		//年齢が0歳だったら
+		if(nenrei==0);
+		 	kakaku=100;
+	
+		 
+		
+			
 
 
 
