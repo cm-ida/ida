b762b820164841494ba39071228488ab2d65a82e
cm-ida <ida@cherimo.co.jp>
2019-09-04 01:21:12 +0900

    付録問題８メイン

---
+++ javaTest/src/javaObject/Toi8.java
@@ -0,0 +1,16 @@
+package javaObject;
+
+public class Toi8 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 3;i ++);
+			
+
+		Toi8Sub toi8sub = new Toi8Sub();
+		toi8sub.KeisanA();
+
+	
+	}
+
+}
---
+++ javaTest/src/javaObject/Toi8Sub.java
@@ -0,0 +1,12 @@
+package javaObject;
+
+public class Toi8Sub {
+	  public void  KeisanA() {
+		 
+	  
+			int a=0;
+			System.out.println("aの値は" + a + "です。");
+			a =a + 1;
+			return;
+		}
+	}
