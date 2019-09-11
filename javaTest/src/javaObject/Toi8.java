2afb2a318fd53bc0740746c111dd8bef94a5f241
cm-ida <ida@cherimo.co.jp>
2019-09-11 21:16:01 +0900

    付録問題８コメント答え、次回の問題

--- javaTest/src/javaObject/Toi8.java
+++ javaTest/src/javaObject/Toi8.java
@@ -4,13 +4,24 @@ public class Toi8 {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i <= 3;i ++);
-			
+		//		for (int i = 1; i < 3;i++);
+		//
+		//
+		//		Toi8Sub toi8sub = new Toi8Sub();
+		//		toi8sub.KeisanA();
 
+//		Toi8Sub toi8sub = new Toi8Sub();
+//		int a = 0;
+//		for (int i = 1; i <= 3; i++) {
+//			a = toi8sub.keisanA(a);
+//		};
+//
 		Toi8Sub toi8sub = new Toi8Sub();
-		toi8sub.KeisanA();
+		for (int i = 1; i <= 3; i++) {
+			toi8sub.keisanA();
+		};
 
-	
+
 	}
 
 }
