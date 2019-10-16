1c7aa7f11f9cbdf7e038dd020f41107161ce3074
cm-ida <ida@cherimo.co.jp>
2019-10-16 19:59:39 +0900

    キャタピー進化プログラム　ちょっと修正版

--- javaTest/src/javaPractice/Poke10.java
+++ javaTest/src/javaPractice/Poke10.java
@@ -1,5 +1,78 @@
 package javaPractice;
 
+import java.util.Scanner;
+
 public class Poke10 {
 
+	public static void main(String[] args) throws Exception {
+
+		// Your code here!
+		String[] ctp = { "キャタピー", "トランセル", "バタフリー" };
+		int level = 1;
+		int shinka = 0;
+		int hp = 10;
+
+		Poke10Sub sp = new Poke10Sub();
+
+		System.out.println(ctp[0] + "を捕まえた。");
+
+		System.out.println(ctp[0] + "のニックネームを入力してね！");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.next();
+
+		System.out.println(ctp[0] + "のニックネームは" + name + "だ！");
+
+		//条件レベル1から100まで
+		for (int a = 1; a >= 1 && a <= 100; a++) {
+
+			//条件分岐shinka = 0の時
+			while (shinka == 0) {
+
+				level++;
+				System.out.println("あ、野生のポケモンが現れた！！");
+
+				//レベルが７より小さければ
+				if (level < 7) {
+
+					sp.levelup(name, level);
+
+				} else {
+
+					sp.levelup(name, level);
+
+					System.out.println("おめでとう!" + name + "は" + ctp[1] + "に進化した！");
+
+					shinka = 1;
+
+				}
+			}
+			//条件分岐shinka = 1の時
+			while (shinka == 1) {
+				
+				level++;
+				System.out.println("あ、野生のポケモンが現れた！！");
+
+				//レベルがより小さい
+				if (level < 10) {
+
+					sp.levelup(name, level);
+
+				} else {
+
+					sp.levelup(name, level);
+					System.out.println("おめでとう!" + name + "は" + ctp[2] + "に進化した！");
+
+					shinka = 2;
+
+				}
+
+			}
+
+		}
+		//   hp = sp.fight
+		//		System.out.println(hp);
+
+		scan.close();
+
+	}
 }
