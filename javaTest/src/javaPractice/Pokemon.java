78056d7fe1575259bf2e94af7ccb759a86e5de35
cm-ida <ida@cherimo.co.jp>
2019-10-31 01:35:26 +0900

    ゲッターセッター途中

---
+++ javaTest/src/javaPractice/Pokemon.java
@@ -0,0 +1,38 @@
+package javaPractice;
+
+public class Pokemon {
+
+//	public void reset() {
+//
+//
+//		pokeName = "caterpie";
+//		level = 100;
+//
+//		pokeName = "pikachu";
+//		level = 60;
+//
+//	}
+
+	private String pokeName;
+	private int level;
+
+	public String getpokeName() {
+		return pokeName;
+
+	}
+
+	public void setpokeName(String pokeName) {
+		this.pokeName = pokeName;
+
+	}
+
+	public int getlevel() {
+		return level;
+
+	}
+
+	public void setlevel(int level) {
+		this.level = level;
+	}
+
+}
