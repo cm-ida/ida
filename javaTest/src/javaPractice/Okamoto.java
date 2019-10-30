3fb879699449cdf41bdcb6ac61ad42bda593401c
cm-ida <ida@cherimo.co.jp>
2019-10-31 01:33:05 +0900

    getter,setter がんばろー！！

---
+++ javaTest/src/javaPractice/Okamoto.java
@@ -0,0 +1,24 @@
+package javaPractice;
+
+import java.util.Scanner;
+public static void main(String[] args) {
+
+public class Okamoto {
+	
+	System.out.println(  "あなたのニックネームを入力してね！");
+
+	
+	Scanner scan = new Scanner(System.in);
+	String name = scan.next();
+
+	System.out.println("あなたのニックネームは" + name + "だ！");
+	
+	System.out.println(name + "お誕生日おめでとう")；
+	
+
+
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
